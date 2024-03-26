af6b2815df769a22a02f668e0fa28997e630a05f
47th0a <47th0a@gmail.com>
2024-03-26 14:37:56 +0800

    test

--- script/yjb.js
+++ script/yjb.js
@@ -1,10 +1,12 @@
-var body = $response['body'];
-var obj = JSON['parse'](body);
+var body = $response.body;
+var obj = JSON.parse(body);
+
 obj['data']['nickname'] = 1;
 obj['data']['vip_label'] = true;
 obj['data']['is_pay'] = true;
 obj['data']['vip_expiry_date'] = 2099-12-31;
-body = JSON['stringify'](obj);
-$done({
-    body
-})
+body = JSON.stringify(obj);
+
+console.log(body);
+
+$done(body);
