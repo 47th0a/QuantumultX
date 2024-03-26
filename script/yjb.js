c660c203f05d3280811839f2d845d805d7c28153
47th0a <47th0a@gmail.com>
2024-03-26 15:51:22 +0800

    update

--- script/yjb.js
+++ script/yjb.js
@@ -5,7 +5,7 @@ obj['data']['nickname'] = 1;
 obj['data']['subscribe_status'] = 1;
 obj['data']['vip_label'] = true;
 obj['data']['is_pay'] = true;
-obj['data']['vip_expiry_date'] = 2099-12-31;
+obj['data']['vip_expiry_date'] = "2099-12-31 09:12:11";
 obj['data']['open_free_vip_sign'] = true;
 body = JSON.stringify(obj);
 
